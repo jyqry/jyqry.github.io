5d6aa1cd8f6d8a522f49fe3236aeeebea0d97780
Chichi Lee <chichi.lee@healingpaper.com>
2021-04-02 03:56:56 +0900

    20210402

--- sw.js
+++ sw.js
@@ -52,7 +52,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "68130f352da9d3a25dfbf02ea4f43fb5"
+    "revision": "14d5ff97a9af7034ca4bcd42b4e8c3e4"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
